fc3de8faad92a93e19e1cca4e25e4ff40cc84405
melkawakibi <melkawakibi@gmail.com>
2014-10-12 15:44:59 +0200

    Project firefalcon werkt! jongen bouwen maar!

---
+++ FireFalcon/src/main/java/com/firefalcon/model/User.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.firefalcon.model;
+
+import javax.persistence.*;
+
+/**
+ *
+ * @author Mohamed
+ */
+@Entity
+public class User {
+    @Id @GeneratedValue
+    @Column(name = "id")
+    private int Id;
+    
+    @Column(name = "username")
+    private String username;
+    
+    @Column(name = "password")
+    private String password;
+    
+    @Column(name = "first name")
+    private String firstName;
+    
+    @Column(name = "last name")
+    private String lastName;
+
+    public User(String username, String password, String firstName, String lastName) {
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+    //contructor for login
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
+    
+    
+    
+}
