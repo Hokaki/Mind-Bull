0c9e278daef2d51188747a4a875f696ff8c2db16
melkawakibi <melkawakibi@gmail.com>
2014-10-22 18:54:26 +0200

    velden validatie(niet werkend)

--- FireFalcon/src/main/java/com/firefalcon/controller/IndexController.java
+++ FireFalcon/src/main/java/com/firefalcon/controller/IndexController.java
@@ -15,12 +15,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value="/index")
 public class IndexController {
         
+        @RequestMapping(value = {"/index"}, method = RequestMethod.GET)
 	public ModelAndView index() throws IOException{
             
             return new ModelAndView("index");
