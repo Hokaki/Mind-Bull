5b65ba1e09e2ec2734b886c51b212a2da574b2e9
Joost96 <joost-k@live.nl>
2014-11-25 11:04:21 +0100

    flamining raven can open files , i think

--- FlamingRaven/src/main/java/com/ewa/flamingraven/Main.java
+++ FlamingRaven/src/main/java/com/ewa/flamingraven/Main.java
@@ -1,5 +1,12 @@
 package com.ewa.flamingraven;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author Joost
@@ -8,6 +15,44 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hallo");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("files\\KniestrekkenAhmed.txt"));
+            String line = null;
+            List<String[]> data = new LinkedList<String[]>();
+            while ((line = reader.readLine()) != null) {
+                String rawData[] = line.split(",");
+                int count=0;
+                String[] temp = null;
+                for(int i=5;i<rawData.length;i++) {
+                    switch (count) {
+                        case 0:
+                            temp[0]=rawData[i];
+                            break;
+                        case 1:
+                            temp[1]=rawData[i];
+                            break;
+                        case 2:
+                            temp[2]=rawData[i];
+                            break;
+                        case 3:
+                            temp[3]=rawData[i];
+                            break;
+                        case 5:
+                            data.add(temp);
+                            count=-1;
+                            break;
 
+                    }
+                    count++;
+                }
+            }
+            for (int i=0;i<data.size();i++) {
+                System.out.println(data.get(i));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- FlamingRaven/src/main/java/com/ewa/flamingraven/dao/AssignmentResultDAO.java
+++ FlamingRaven/src/main/java/com/ewa/flamingraven/dao/AssignmentResultDAO.java
@@ -1,7 +1,60 @@
 package com.ewa.flamingraven.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by Joost on 11/18/2014.
  */
+
+@Repository
 public class AssignmentResultDAO {
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private Session getCurrentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    public void addUser(User user) {
+        getCurrentSession().save(user);
+    }
+
+    public void updateUser(User user) {
+        User userToUpdate = getUser(user.getUsername());
+        userToUpdate.setUsername(user.getUsername());
+        userToUpdate.setPassword(user.getPassword());
+        userToUpdate.setFirstName(user.getFirstName());
+        userToUpdate.setLastName(user.getLastName());
+
+        getCurrentSession().update(userToUpdate);
+
+    }
+
+    public User getUser(String userName) {
+        User user = (User) getCurrentSession().get(User.class, userName);
+        return user;
+    }
+
+    public void deleteUser(String userName) {
+        User user = getUser(userName);
+        if (user != null) {
+            getCurrentSession().delete(user);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<User> getUsers() {
+        return getCurrentSession().createQuery("from User").list();
+    }
+
+    public void storeAllUsers(List<User> users) {
+
+        for (User user : users) {
+            getCurrentSession().save(user);
+        }
+
+    }
 }
